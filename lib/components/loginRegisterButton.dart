2d4b9f76642ba5fe93ffee304fe4d2530bbd1165
ArafatRohan93 <rohanarafat86@gmail.com>
2020-06-25 16:58:38 +0600

    ⚡️

---
+++ lib/components/loginRegisterButton.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class LoginRegisterButton extends StatelessWidget {
+  const LoginRegisterButton({@required this.color, @required this.text, @required this.onPress});
+
+  final Color color;
+  final String text;
+  final Function onPress;
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 16.0),
+      child: Material(
+        elevation: 5.0,
+        color: color,
+        borderRadius: BorderRadius.circular(30.0),
+        child: MaterialButton(
+          onPressed: onPress,
+          minWidth: 200.0,
+          height: 42.0,
+          child: Text(
+            text,
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+      ),
+    );
+  }
+}
